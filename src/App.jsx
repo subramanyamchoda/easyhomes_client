7a741bc1a8c544eab49d035c35c9437ba73cee32
Choda Subramanyam <149325423+subramanyamchoda@users.noreply.github.com>
2025-04-21 11:13:39 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes ,Link} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes ,useNavigate} from "react-router-dom";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Register from "./components/Register";
