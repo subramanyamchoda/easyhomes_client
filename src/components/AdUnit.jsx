8e79922549bc8ae21f8119b23ab96cf150948b24
Choda Subramanyam <149325423+subramanyamchoda@users.noreply.github.com>
2025-04-25 17:19:41 +0530

    Update AdUnit.jsx

--- src/components/AdUnit.jsx
+++ src/components/AdUnit.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+  import { useEffect, useRef } from 'react';
 
 export default function AdUnit({ slot, format = 'auto', responsive = true }) {
   const adRef = useRef(null);
@@ -14,14 +14,15 @@ export default function AdUnit({ slot, format = 'auto', responsive = true }) {
   }, [slot]);
 
   return (
-    <ins
-      className="adsbygoogle"
-      style={{ display: 'block' }}
-      data-ad-client="ca-pub-1417536970473743"
-      data-ad-slot={slot}
-      data-ad-format={format}
-      data-full-width-responsive={responsive ? 'true' : 'false'}
-      ref={adRef}
-    ></ins> // Use regular open/close tag for <ins> in JSX
+<ins
+  className="adsbygoogle"
+  style={{ display: 'block' }}
+  data-ad-client="ca-pub-1417536970473743"
+  data-ad-slot={slot}
+  data-ad-format={format}
+  data-full-width-responsive={responsive ? 'true' : 'false'}
+  ref={adRef}
+/>
+ // Use regular open/close tag for <ins> in JSX
   );
 }
