0d8367fc7d06b654d57a0cf91824219f3ae42dd2
Choda Subramanyam <149325423+subramanyamchoda@users.noreply.github.com>
2025-04-25 17:23:12 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
+
 export default defineConfig({
   plugins: [
-    tailwindcss(),
+    react({
+      jsxRuntime: 'automatic', // Enables modern JSX transformation
+    }),
+    tailwindcss()
   ],
-})
+});
